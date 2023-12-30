9c3116cb8c9520d7f20b8b813f25b54d1d271b19
SShahid97 <sshahidmalik97@gmail.com>
2023-12-30 16:53:30 +0530

    chore: fix ui of about section for mobile devices

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,13 +8,16 @@ const About = ({about}) => {
       >
         About
       </div>
-      <div className="flex flex-col max-w-[1000px] md:mx-auto mx-3 gap-1 text-justify">
+      <div className="flex flex-col max-w-[1000px] md:mx-auto mx-3 gap-15 md:gap-1 text-justify">
         {about && about.map((paragraph)=>{
             return (
               <div 
                 key={paragraph.order} 
                 className="
-                  w-[48%] 
+                  w-auto
+                  md:w-[48%]
+                  mb-3
+                  md:mb-0
                   rounded-2xl 
                   p-4 
                   border-gray-200 
@@ -24,7 +27,8 @@ const About = ({about}) => {
                   even:rotate-2
                   odd:-rotate-2
                   hover:rotate-0
-                  hover:scale-110
+                  hover:scale-105
+                  md:hover:scale-110
                   duration-500
                   "
               >
